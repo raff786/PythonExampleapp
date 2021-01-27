b134ff0fcf4e54f4c7e0fc5bec460a39dcd3af13
Atiq61299 <atiqr95@hotmail.com>
2021-01-27 11:48:11 +0000

    updated 8

--- python-example-app/app.py
+++ python-example-app/app.py
@@ -593,9 +593,9 @@ async def main(loop):
     server = await loop.create_server(app.make_handler(), '0.0.0.0', port)
 
     print('Listening on port {}'.format(port))
-   # await loop.create_task(example(loop))
+    await loop.create_task(example(loop))
 
-    return web.Response(text='Success')
+    #return web.Response(text='Success')
 
 
 if __name__ == '__main__':
