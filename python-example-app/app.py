7f74c459bed16cdcb7ec59b5743fbb5ad9dd16be
Atiq61299 <atiqr95@hotmail.com>
2021-01-27 10:57:20 +0000

    test1

--- python-example-app/app.py
+++ python-example-app/app.py
@@ -1,5 +1,32 @@
-def sum_2(a, b):
-    return a+b
+# def sum_2(a, b):
+#     return a+b
+#
+#
+# sum_2(5, 6)
 
+import logging
+import os
+import pyqrcode
+import requests
+import traceback
+import asyncio
+from asyncio.base_events import Server
+from aiohttp import web
+from aiohttp.web_routedef import RouteTableDef
+from verity_sdk.handlers import Handlers
+from verity_sdk.protocols.v0_6.IssuerSetup import IssuerSetup
+from verity_sdk.protocols.v0_6.UpdateConfigs import UpdateConfigs
+from verity_sdk.protocols.v0_6.UpdateEndpoint import UpdateEndpoint
+from verity_sdk.protocols.v0_6.WriteCredentialDefinition import WriteCredentialDefinition
+from verity_sdk.protocols.v0_6.WriteSchema import WriteSchema
+from verity_sdk.protocols.v0_7.Provision import Provision
+from verity_sdk.protocols.v1_0.Connecting import Connecting
+from verity_sdk.protocols.v1_0.IssueCredential import IssueCredential
+from verity_sdk.protocols.v1_0.PresentProof import PresentProof
+from verity_sdk.protocols.v1_0.Relationship import Relationship
+from verity_sdk.protocols.v1_0.CommittedAnswer import CommittedAnswer
+from verity_sdk.utils.Context import Context
+from indy.wallet import delete_wallet
+from indy.error import WalletNotFoundError
 
-sum_2(5, 6)
+from helper import *
